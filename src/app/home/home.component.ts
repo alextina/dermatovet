3cc106f81b8e5bcb83af1b82f4cee1891c3e265d
alextina <castilloavilaa@gmail.com>
2023-06-14 23:14:35 -0500

    Maquetando y colocando estilos al AseoComponent - Agregando estilos, componente responsive - Agregando modal para detalles de los servicios - Agregando botón de WhatsApp para cada servicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,26 @@ export class HomeComponent {
 
   constructor(
     public sharedSvc: SharedServiceService,
-  ) {
+  ) { }
 
-  }
+  services = [
+    {
+      "title": "Therapy Clean",
+      "image": "../../assets/therapy-clean.png"
+
+    },
+    {
+      "title": "Ozonoterapia",
+      "image": "../../assets/ozonoterapia.png"
+    },
+    {
+      "title": "Baño Medicado",
+      "image": "../../assets/baños-medicados.png"
+    },
+    {
+      "title": "Baño Clásico",
+      "image": "../../assets/baños-clásicos.png"
+    }
+  ]
 
 }
